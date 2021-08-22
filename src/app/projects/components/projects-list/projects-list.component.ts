3de0e98ad6c4bbecb2984703f788d47fd361d9a4
Juansecu <juansecoder@hotmail.com>
2021-08-22 10:40:43 -0500

    Template and functionality for ProjectsListComponent from ProjectsModule were implemented

--- src/app/projects/components/projects-list/projects-list.component.ts
+++ src/app/projects/components/projects-list/projects-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IProject } from '../../typings/Project';
+
+import { ProjectsService } from '../../services/projects.service';
+
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.scss']
 })
 export class ProjectsListComponent implements OnInit {
+  projects: IProject[] = [];
 
-  constructor() { }
+  constructor(private readonly _PROJECTS_SERVICE: ProjectsService) {}
 
   ngOnInit(): void {
+    this.getProjects();
   }
 
+  getProjects() {
+    this._PROJECTS_SERVICE
+      .getProjects()
+      .subscribe(projects => (this.projects = projects));
+  }
 }
--- src/app/projects/services/projects.service.ts
+++ src/app/projects/services/projects.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { IProject } from '../typings/Project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
+  private readonly _BASE_Url = environment.serverUri;
 
-  constructor() { }
+  constructor(private readonly _HTTP_CLIENT: HttpClient) {}
+
+  getProjects(): Observable<IProject[]> {
+    return this._HTTP_CLIENT.get<IProject[]>(`${this._BASE_Url}/projects`);
+  }
 }

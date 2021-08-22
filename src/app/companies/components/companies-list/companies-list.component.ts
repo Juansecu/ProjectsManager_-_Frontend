827deb950cbf6b0b0fa9ae9f9af61f871446545a
Juansecu <juansecoder@hotmail.com>
2021-08-22 10:22:38 -0500

    Template and functionality for CompaniesListComponent from CompaniesModule were setted

--- src/app/companies/components/companies-list/companies-list.component.ts
+++ src/app/companies/components/companies-list/companies-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICompany } from '../../typings/Company';
+
+import { CompaniesService } from '../../services/companies.service';
+
 @Component({
   selector: 'app-companies-list',
   templateUrl: './companies-list.component.html',
   styleUrls: ['./companies-list.component.scss']
 })
 export class CompaniesListComponent implements OnInit {
+  companies: ICompany[] = [];
 
-  constructor() { }
+  constructor(private readonly _COMPANIES_SERVICE: CompaniesService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getCompanies();
   }
 
+  getCompanies() {
+    this._COMPANIES_SERVICE
+      .getCompanies()
+      .subscribe(companies => (this.companies = companies));
+  }
 }
--- src/app/companies/services/companies.service.ts
+++ src/app/companies/services/companies.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { ICompany } from '../typings/Company';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
+  private readonly _BASE_URL = environment.serverUri;
 
-  constructor() { }
+  constructor(private readonly _HTTP_CLIENT: HttpClient) {}
+
+  getCompanies(): Observable<ICompany[]> {
+    return this._HTTP_CLIENT.get<ICompany[]>(`${this._BASE_URL}/companies`);
+  }
 }

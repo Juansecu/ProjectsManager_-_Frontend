6a125f0ad243f2cb1d0815f86ee5f0c473e14e49
Juansecu <juansecoder@hotmail.com>
2021-08-22 07:47:13 -0500

    ILoginUser was added into User typings from AuthModule

--- src/app/auth/typings/User.d.ts
+++ src/app/auth/typings/User.d.ts
@@ -1,7 +1,12 @@
-export interface AddUser {
+export interface IAddUser {
   username: string;
   email: string;
   firstName: string;
   lastName: string;
+  password: string;
+}
+
+export interface ILoginUser {
+  username: string;
   password: string;
 }

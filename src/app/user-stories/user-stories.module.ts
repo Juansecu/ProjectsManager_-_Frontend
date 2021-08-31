2311bd7804c0579dc7e64f8c08090e91697ecae0
Juansecu <juansecoder@hotmail.com>
2021-08-31 17:30:20 -0500

    UserStoriesModule was added with its own routing module

---
+++ src/app/user-stories/user-stories.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserStoriesRoutingModule } from './user-stories-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, UserStoriesRoutingModule]
+})
+export class UserStoriesModule {}

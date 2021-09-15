a66efde7fb9f6eb4b482980c1784618b64305984
Juansecu <juansecoder@hotmail.com>
2021-09-15 11:52:12 -0500

    Routes were configured to be displayed through NavbarComponent dynamically

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Route } from '../../typings/Route';
+
+import { routesConstant } from '../../constants/routes.constant';
+
+import { AuthService } from 'src/app/auth/services/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  routes: Route[] = routesConstant;
 
-  constructor() { }
+  constructor(private readonly _AUTH_SERVICE: AuthService) {}
 
   ngOnInit(): void {
+    if (!this._AUTH_SERVICE.verifyToken()) {
+      this.routes.push({
+        label: 'Login',
+        path: '/auth/login'
+      });
+      this.routes.push({
+        label: 'Register',
+        path: '/auth/register'
+      });
+    } else {
+      this.routes.push({
+        label: 'Tickets',
+        path: '/tickets'
+      });
+    }
   }
-
 }
---
+++ src/app/shared/constants/routes.constant.ts
@@ -0,0 +1,12 @@
+import { Route } from '../typings/Route';
+
+export const routesConstant: Route[] = [
+  {
+    label: 'Projects',
+    path: '/projects'
+  },
+  {
+    label: 'Companies',
+    path: '/companies'
+  }
+];
